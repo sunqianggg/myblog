1162038cae169076eb709cddf1aa58d461cbb38d
sunqianggg <sunqiangsworld@gmail.com>
2017-01-06 15:52:19 +0800

    add login model

---
+++ app/email.py
@@ -0,0 +1,24 @@
+from flask_mail import Message
+from flask import current_app,render_template
+from . import mail
+from threading import Thread
+
+
+
+# def send_email(to,subject,template,**kwargs):
+#     msg=Message(current_app.config['FLASKY_MAIL_SUBJECT_PREFIX']+subject,sender=current_app['FLASKY_MAIL_SEND'],recipients=[to])
+#     msg.body=render_template(template+'.txt',**kwargs)
+#     msg.html=render_template(template+'.html',**kwargs)
+#     mail.send(msg)
+
+def send_async_email(app,msg):
+    with current_app.app_context():
+        mail.send(msg)
+
+def send_email(to,subject,template,**kwargs):
+    msg=Message(current_app.config['FLASKY_MAIL_SUBJECT_PREFIX']+subject,sender=current_app.config['FLASKY_MAIL_SENDER'],recipients=[to])
+    msg.body=render_template(template+'.txt',**kwargs)
+    msg.html=render_template(template+'.html',**kwargs)
+    thr=Thread(target=send_async_email,args=[current_app,msg])
+    thr.start()
+    return thr
--- app/models.py
+++ app/models.py
@@ -2,6 +2,9 @@ from . import db
 from werkzeug.security import  generate_password_hash,check_password_hash
 from flask_login import UserMixin
 from . import login_manager
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from flask import current_app
+
 
 
 @login_manager.user_loader
@@ -14,7 +17,7 @@ class Role(db.Model):
     name = db.Column(db.String(64), unique=True)
     user = db.relationship('User', backref='role')
 
-    def __repr__j(self):
+    def __repr__(self):
         return "<Role %r>" % self.name
 
 
@@ -25,6 +28,7 @@ class User(UserMixin,db.Model):
     username = db.Column(db.String(64), unique=True, index=True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     password_hash=db.Column(db.String(128))
+    confirmed=db.Column(db.Boolean,default=False)
 
     @property
     def password(self):
@@ -36,6 +40,22 @@ class User(UserMixin,db.Model):
 
     def verify_password(self,password):
         return check_password_hash(self.password_hash,password)
+
+    def generate_confirmation_token(self,expiraton=3600):
+        s=Serializer(current_app.config['SECRET_KEY'],expiraton)
+        return s.dumps({'confirm':self.id})
+
+    def confirm(self,token):
+        s=Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data=s.loads(token)
+        except:
+            return False
+        if data.get('confirm')!=self.id:
+            return False
+        self.confirmed=True
+        db.session.add(self)
+        return True
 
     def __repr__(self):
         return '<User %r>' % self.username
